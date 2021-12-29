392b16ca5368b0bba43fccf77a66304af06118dd
Rain <448130604@qq.com>
2021-12-29 17:16:07 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 var path = require('path');
 
 module.exports = {
@@ -5,6 +7,20 @@ module.exports = {
     entry: './src/index.js',  //入口
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js'  //出口
+        filename: 'index[contenthash].js'  //出口
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Rain',  //html的title
+        template: 'src/assets/index.html'
+    })], //自动生成html
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
     }
+
 };
+
